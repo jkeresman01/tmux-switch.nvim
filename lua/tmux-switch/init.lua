e854f5ff1a63c67037432f0b174e2f8e39773a7d
josip <josipkeresman@gmail.com>
2024-07-20 17:26:41 +0200

    Implement create new session option

--- lua/tmux-switch/init.lua
+++ lua/tmux-switch/init.lua
@@ -1,38 +1,79 @@
 local util = require('tmux-switch.util')
 
-local telescope_actions      = require('telescope.actions')
-local telescope_action_state = require('telescope.actions.state')
-local telescope_finders      = require('telescope.finders')
-local telescope_pickers      = require('telescope.pickers')
-local telescope_sorters      = require('telescope.sorters')
-local telescope_themes       = require('telescope.themes')
+local actions = require('telescope.actions')
+local action_state = require('telescope.actions.state')
+local finders = require('telescope.finders')
+local pickers = require('telescope.pickers')
+local sorters = require('telescope.sorters')
+local themes = require('telescope.themes')
+
+local nui_input = require('nui.input')
+local event = require("nui.utils.autocmd").event
 
 local M = {}
 
 function M.switch()
     local tmux_sessions = util.get_tmux_sessions()
-    local opts = telescope_themes.get_dropdown({})
+    local opts = themes.get_dropdown({})
 
-    telescope_pickers.new(opts, {
-        prompt_title = 'TMUX sessions',
+    pickers.new(opts, {
+        prompt_title = "TMUX switch",
 
-        finder = telescope_finders.new_table {
+        finder = finders.new_table {
             results = tmux_sessions,
         },
 
-        sorter = telescope_sorters.get_generic_fuzzy_sorter(opts),
+        sorter = sorters.get_generic_fuzzy_sorter(opts),
 
         attach_mappings = function(_, map)
-            map('i', '<CR>', function(prompt_bufnr)
-                local selected_session = telescope_action_state.get_selected_entry()
-                telescope_actions.close(prompt_bufnr)
-                vim.fn.system('tmux switch -t ' .. selected_session.value)
+            map("i", "<CR>", function(prompt_bufnr)
+                local selected_session = action_state.get_selected_entry()
+                actions.close(prompt_bufnr)
+                util.switch_to_session(selected_session.value)
             end)
             return true
         end,
 
     }):find()
 end
+
+function M.create_session()
+    local input = nui_input({
+        position = "50%",
+
+        size = {
+            width = 40,
+        },
+
+        border = {
+            style = "rounded",
+            text = {
+                top = "TMUX switch",
+                top_align = "center",
+            },
+        },
+
+        win_options = {
+            winhighlight = "Normal:Normal,FloatBorder:Normal",
+        },
+    }, {
+        prompt = "> ",
+        default_value = "",
+
+        on_submit = function(session_name)
+            util.create_new_session(session_name)
+            util.switch_to_session(session_name)
+        end,
+    })
+
+    input:mount()
+
+    input:on(event.BufLeave, function()
+        input:unmount()
+    end)
+end
+
+-- TODO quick switch implementation
 
 return M
 
--- lua/tmux-switch/util.lua
+++ lua/tmux-switch/util.lua
@@ -1,11 +1,11 @@
 local M = {}
 
 function M.get_tmux_sessions()
-    local list_tmux_sessions = "tmux ls | awk -F ':' '{print $1}'"
-    local handle = io.popen(list_tmux_sessions)
+    local list_tmux_sessions_cmd = "tmux ls | awk -F ':' '{print $1}'"
+    local handle = io.popen(list_tmux_sessions_cmd)
 
     if not handle then
-        vim.notify("No can do!")
+        vim.notify "No can do for getting tmux sessions!"
         return {}
     end
 
@@ -18,6 +18,36 @@ function M.get_tmux_sessions()
     end
 
     return tmux_sessions
+end
+
+-- TODO use for quick switch 
+function M.get_current_tmux_session()
+    local display_current_tmux_session_cmd = "tmux display-message -p '#S'"
+    local handle = io.popen(display_current_tmux_session_cmd)
+
+    if not handle then
+        vim.notify "No can do for get current tmux session!"
+        return nil
+    end
+
+    local result = handle:read("*a")
+    handle:close()
+
+    return result:match("^%s*(.-)%s*$")
+end
+
+function M.switch_to_session(session_name)
+    if session_name then
+        local switch_to_session_cmd = string.format("tmux switch -t %s", session_name)
+        vim.fn.system(switch_to_session_cmd)
+    end
+end
+
+function M.create_new_session(session_name)
+    if session_name and session_name ~= "" then
+        local create_new_session_cmd = string.format("tmux new-session -d -s %s", session_name)
+        vim.fn.system(create_new_session_cmd)
+    end
 end
 
 return M
