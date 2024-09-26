161787eb00284be43170ae731d42259c4c620f3a
josip <josipkeresman@gmail.com>
2024-09-26 17:54:03 +0200

    Fix commands description

--- lua/tmux-switch/commands.lua
+++ lua/tmux-switch/commands.lua
@@ -7,19 +7,19 @@ function M.register()
     vim.api.nvim_create_user_command("TmuxSwitch", function()
         tmux.switch()
     end, {
-        desc = "Run Java test picker",
+        desc = "Run Tmux switch picker",
     })
 
     vim.api.nvim_create_user_command("TmuxCreateSession", function()
         tmux.create_session()
     end, {
-        desc = "Execute test at cursor position",
+        desc = "Create new tmux session",
     })
 
     vim.api.nvim_create_user_command("TmuxRenameSession", function()
         tmux.rename_session()
     end, {
-        desc = "Run all tests in the current Java class",
+        desc = "Rename current tmux session",
     })
 end
 
