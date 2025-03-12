51e67c8f6aeeb0255bc4d76f59cfe46041fd79b0
SirSimon04 <simi@engelnetz.de>
2025-03-12 13:31:48 +0100

    fix(tools): Remove current session from list

--- lua/tmux-switch/util.lua
+++ lua/tmux-switch/util.lua
@@ -17,7 +17,6 @@ local M = {}
 function M.get_tmux_sessions(config)
     local list_tmux_sessions_cmd
     if config and config.sort_by_recent_use then
-        vim.notify("sort by recent use")
         list_tmux_sessions_cmd =
             "tmux ls -F '#{session_last_attached} #{session_name}' | sort -nr | awk '{print $2}'"
     else
@@ -33,9 +32,16 @@ function M.get_tmux_sessions(config)
     local tmux_ls_results = handle:read("*a")
     handle:close()
 
+    local current_session_cmd = "tmux display-message -p '#S'"
+    local handle_current = io.popen(current_session_cmd)
+    local current_session = handle_current:read("*a"):gsub("\n", "")
+    handle_current:close()
+
     local tmux_sessions = {}
     for session in string.gmatch(tmux_ls_results, "[^\n]+") do
-        table.insert(tmux_sessions, session)
+        if session ~= current_session then
+            table.insert(tmux_sessions, session)
+        end
     end
 
     return tmux_sessions
